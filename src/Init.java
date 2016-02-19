1ef006fb2fb2f53493214ae74ea83f24e8a75608
tommyjin <tommyjing0620@gmail.com>
2016-02-19 00:55:37 +0000

    third

---
+++ src/Init.java
@@ -0,0 +1,15 @@
+import com.google.gson.Gson;
+
+/**
+ * Created by tommy on 2016/2/18.
+ */
+public class Init {
+    public void createJsonFile(){
+        Gson gson = new Gson();
+
+
+
+
+
+    }
+}
